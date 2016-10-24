90b315c2a9ab4fd73725c8a7df7e5ee4386c28ec
Jerry Zou <jerry.zry@outlook.com>
2016-10-25 00:56:08 +0800

    Done.

--- src/benchmark/flexNav.js
+++ src/benchmark/flexNav.js
@@ -1,53 +1,72 @@
+const fs = require('fs');
 const Quadtree = require('../structures/Quadtree');
-const { generateAvailablePoint, generateObstacles } = require('./utils');
+const { generateAvailablePoint, generateMap } = require('./utils');
 const { FlexNav, Point } = require('../FlexNav');
+const mapData = require('./mapData');
 
-function test(w, h, obstacleNum, limitTime, count) {
+try {
+  fs.statSync('./output');
+} catch (e) {
+  fs.mkdirSync('./output');
+}
+
+let fd = fs.openSync(`./output/flexNav-${Date.now()}.csv`, 'a');
+
+log('Limit Time, On Time, No Path, Unavailable, Level');
+
+const limitTimes = [2, 5, 10, 20, 50, 200, 1000];
+const categories = ['sparse', 'narrow', 'dense'];
+const indices = [0, 0, 0];
+for (let i = 12; i <= 12; i++) {
+  categories.forEach((c, id) => {
+    let w = Math.pow(2, i);
+    log(`Map Width: ${w}`);
+    log(`Category: ${c}`);
+    test(w, w, mapData[c][indices[id]], limitTimes, 10);
+  })
+}
+
+fs.closeSync(fd);
+
+function log(str) {
+  fs.writeSync(fd, str + '\n');
+  console.log(str);
+}
+
+function test(w, h, data, limitTimes, count) {
   let map = new Quadtree({
     width: w,
     height: h,
     maxDepth: 12
   });
   
-  generateObstacles(map, obstacleNum);
+  generateMap(map, data);
 
   let nav = new FlexNav(map);
-  let onTime = 0;
-  let noPath = 0;
-  let unavailable = 0;
 
-  for (let i = 0; i < count; i++) {
-    let start = generateAvailablePoint(map, [0, 1 / 4], [0, 1 / 4]);
-    let end = generateAvailablePoint(map, [3 / 4, 1], [3 / 4, 1]);
-    let res = nav.search(start, end, limitTime);
-    switch (res.status) {
-      case "unavailable":
-        unavailable++;
-        break;
-      case "success":
-        if (res.time < limitTime) onTime++;
-        break;
-      case "noPath":
-        noPath++;
-        break;
+  for (let j = 0; j < limitTimes.length; j++) {
+    let limitTime = limitTimes[j];
+    let onTime = 0;
+    let noPath = 0;
+    let unavailable = 0;
+    let level = 0;
+
+    for (let i = 0; i < count; i++) {
+      let start = generateAvailablePoint(map, [0, 1 / 4], [0, 1 / 4]);
+      let end = generateAvailablePoint(map, [3 / 4, 1], [3 / 4, 1]);
+      let res = nav.search(start, end, limitTime);
+      level = res.level;
+      switch (res.status) {
+        case "unavailable":
+          unavailable++;
+          break;
+        case "noPath":
+          noPath++;
+        case "success":
+          if (res.time < limitTime) onTime++;
+          break;
+      }
     }
-  }
-
-  return {
-    onTime: onTime / count,
-    noPath: noPath / count,
-    unavailable: unavailable / count
-  }
-}
-
-const limitTimes = [5, 10, 20, 40, 100];
-for (let i = 12; i <= 12; i++) {
-  for (let j = 10; j <= 10; j+=3) {
-    let w = Math.pow(2, i);
-    let result;
-    for (let k = 0, n = limitTimes.length; k < n; k++) {
-      result = test(w, w, j, limitTimes[k], 10);
-      console.log(`Map(${w}, ${w})\tObstacles: ${j}\tlimit time: ${limitTimes[k]}\ton time: ${result.onTime}\tnoPath: ${result.noPath}\tunavailable: ${result.unavailable}`);      
-    }
+    log(`${limitTime}, ${onTime / count}, ${noPath / count}, ${unavailable / count}, ${level}`);
   }
 }
